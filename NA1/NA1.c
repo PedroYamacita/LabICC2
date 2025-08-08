a208c3469f2b0adefe1adca5c469935016a1f6e4
pedroYamacita <pedro.teidi@gmail.com>
2025-08-08 17:10:48 -0300

    Atividade 1

--- NA1/NA1.c
+++ NA1/NA1.c
@@ -1,13 +1,40 @@
+// Dupla Leonardo Kenzo e Pedro Yamacita.
 #include <stdio.h>
-typedef struct cidade{
+#include <string.h>
+
+typedef struct cidade
+{
     char nome[100];
-    float populacao;
-}cidade;
-int main(){
-    int nCidades;
+    int populacao;
+} Cidade;
+
+int main()
+{
+
+    int nCidades, populacaoTemp;
+    char cidadeTemp[100];
+
     scanf(" %d", &nCidades);
-    cidade cidadeMaior;
-    for(int i=0; i<nCidades; i++){
-        scanf(" %[^\n]\n %f", );
+    if (nCidades > 10)
+    {
+        printf("n fora do intervalo.");
+        return 0;
     }
+    Cidade cidadeMaior;
+
+    scanf(" %[^\n\r]", cidadeMaior.nome);
+    scanf(" %d", &cidadeMaior.populacao);
+
+    for (int i = 0; i < nCidades - 1; i++)
+    {
+        scanf(" %[^\n\r]", cidadeTemp);
+        scanf(" %d", &populacaoTemp);
+        if (populacaoTemp > cidadeMaior.populacao)
+        {
+            cidadeMaior.populacao = populacaoTemp;
+            strcpy(cidadeMaior.nome, cidadeTemp);
+        }
+    }
+    printf("%s %d", cidadeMaior.nome, cidadeMaior.populacao);
+    return 0;
 }
