6529fdb38c7b685177f805e6e3c2eca8129820a8
pedroYamacita <pedro.teidi@gmail.com>
2025-08-08 16:33:51 -0300

    Agrupamento em pastas

---
+++ NA1/NA1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+typedef struct cidade{
+    char nome[100];
+    float populacao;
+}cidade;
+int main(){
+    int nCidades;
+    scanf(" %d", &nCidades);
+    cidade cidadeMaior;
+    for(int i=0; i<nCidades; i++){
+        scanf(" %[^\n]\n %f", );
+    }
+}
---
+++ NA1/README.txt
@@ -0,0 +1,3 @@
+Arquivo zip gerado em: 08/08/2025 16:33:01 
+Este arquivo contém os casos de teste cadastrados até o momento, disponibilizado pelo professor aos alunos.
+Exercício: [08-08] Nao-avaliativo 1
