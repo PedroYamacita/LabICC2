6d9327acfbdf71beeafea254a9ab8e45dfb410f3
Leonardo Kenzo <leokenzo2006.tanaka@gmail.com>
2025-10-07 13:53:13 -0300

    add/Adicionado alguns sorts

--- Avaliativa4/Entrega4.c
+++ Avaliativa4/Entrega4.c
@@ -4,91 +4,243 @@
 #include <stdbool.h>
 
 // Briqnuedos da AABK
-typedef struct no
-{
+typedef struct brinquedo{
     int id;
     char cor[10];
     float comprimento;
     float nota;
-    struct no *proximo;
-    struct no *anterior;
-} ITEM;
-ITEM *item_criar(int id, char *cor, float comprimento, float nota);
+} BRINQUEDO;
 
-// Lista a ser ordenada
-typedef struct lista
-{
-    ITEM *inicio;
-    ITEM *fim;
+BRINQUEDO *ItemCriar(int id, char *cor, float comprimento, float nota);
+
+// Lista Sequencial Dinamica
+typedef struct lista{
+    BRINQUEDO **elementos;
     int tamanho;
 } LISTA;
-LISTA *lista_criar();
-LISTA *lista_adicionar(LISTA *lista, ITEM *brinquedo);
+
+LISTA *ListaCriar(int brinquedos);
+LISTA *ListaAdicionar(LISTA *lista, BRINQUEDO *brinquedo, int posicao);
+void ListaFree(LISTA **item);
 
 // Algoritmos de ordenação
 void BubbleSort(LISTA *lista);
-void SuickSort(LISTA *lista);
-void MergeSort(LISTA *lista);
+void QuickSort(LISTA *lista);
+void MergeSort(LISTA *lista, int incicio, int fim);
 void InsertionSort(LISTA *lista);
 
 // Função auxiliar
-bool Troca(ITEM *item1, ITEM *item2);
+bool PodeTrocar(BRINQUEDO *item1, BRINQUEDO *item2);
 
 int main()
 {
-    int brinquedos;
+    int brinquedos, comando;
     float nota, comprimento;
     char cor[10];
     scanf(" %d", &brinquedos);
-    LISTA *lista = lista_criar(brinquedos);
-
+    LISTA *lista = ListaCriar(brinquedos);
+    
+    // Leitura dos brinquedos
     for (int i = 0; i < brinquedos; i++)
     {
         scanf(" %s %f %f", cor, &comprimento, &nota);
 
-        ITEM *brinquedo = item_criar(i, cor, comprimento, nota);
-        lista_adicionar(lista, brinquedo);
+        BRINQUEDO *item = ItemCriar(i, cor, comprimento, nota);
+        ListaAdicionar(lista, item, i);
     }
+
+    // Seleciona qual Sort utilizar para ordenacao
+    scanf(" %d", &comando);
+    switch(comando){
+        case 1:
+            BubbleSort(lista);
+            break;
+        case 2:
+            QuickSort(lista);
+            break;
+        case 3:
+            MergeSort(lista, 0, lista->tamanho);
+            break;
+        case 4:
+            InsertionSort(lista);
+            break;
+    }
+
+    // Printa a lista ordenada
+    for(int i = 0; i < brinquedos; i++){
+        printf("%d;", lista->elementos[i]->id);
+    }
+    printf("\n");
+
+    ListaFree(&lista);
     return 0;
 }
 
-LISTA *lista_criar()
+void BubbleSort(LISTA *lista)
 {
+    bool estaOrdenado = false;
+    for (int i = 0; i < lista->tamanho && !estaOrdenado; i++)
+    {
+        for (int j = 0; j < lista->tamanho - 1; j++)
+        {
+
+        }
+    }
+}
+
+void QuickSort(LISTA *lista){
+
+}
+
+// Funcao auxiliar para o MergeSort
+void Merge(LISTA *lista, int inicio, int meio, int fim){
+    int quantElem1 = meio - inicio + 1;
+    int quantElem2 = fim - meio;
+
+    // Cria lista temporarias
+    LISTA *listaTemp1 = ListaCriar(quantElem1);
+    LISTA *listaTemp2 = ListaCriar(quantElem2);
+
+    for(int i = 0; i < listaTemp1->tamanho; i++){
+        listaTemp1->elementos[i] = lista->elementos[inicio + i];
+    }
+    for(int i = 0; i < listaTemp2->tamanho; i++){
+        listaTemp2->elementos[i] = lista->elementos[meio + 1 + i];
+    }
+
+    
+    printf("Entrou no Conquistar\n");
+    
+    // Junta as duas listas ordenadamente comparando os primeiros elementos de cada
+    int i = 0, j = 0;
+    while(i < quantElem1 && j < quantElem2){
+        if(PodeTrocar(listaTemp1->elementos[i], listaTemp2->elementos[j])){
+            lista->elementos[inicio] = listaTemp2->elementos[j];
+            j++;
+        }
+        else{
+            lista->elementos[inicio] = listaTemp1->elementos[i];
+            i++;
+        }
+        inicio++;
+    }
+
+    // Se sobrou elementos, adiciona na lista principal
+    if(i < quantElem1){
+        for(int k = i; k < quantElem1; k++){
+            lista->elementos[inicio] = listaTemp1->elementos[k];
+            inicio++;
+        }
+    }
+    else if(j < quantElem2){
+        for(int k = j; k < quantElem2; k++){
+            lista->elementos[inicio] = listaTemp2->elementos[k];
+            inicio++;
+        }
+    }
+
+    printf("Entrou no fim do Merge\n");
+    // Libera as listas temporarias
+    ListaFree(&listaTemp1);
+    ListaFree(&listaTemp2);
+}
+
+void MergeSort(LISTA *lista, int inicio, int fim){
+    // Condicao de parada da recursao
+    if(inicio < fim){
+        int meio = (inicio + fim)/2;
+        printf("Entrou no MergeSort\nInicio: %d, Meio: %d, Fim: %d\n", inicio, meio, fim);
+        //Dividir
+        MergeSort(lista, inicio, meio);
+        MergeSort(lista, meio + 1, fim);
+
+        // Conquistar
+        Merge(lista, inicio, meio, fim);
+    }
+    printf("Terminou o Merge Sort\n");
+}
+
+
+void InsertionSort(LISTA *lista){
+    int j;
+    for(int i = 1; i < lista->tamanho; i++){
+        // Pega uma chave fixa para comparar
+        BRINQUEDO *chave = lista->elementos[i];
+        j = i - 1;
+
+        // Desloca todos os elementos maiores que o brinquedo chave uma casa para a frente
+        while(j >= 0 && PodeTrocar(lista->elementos[j], chave)){
+            lista->elementos[j + 1] = lista->elementos[j];
+            j--;
+        }
+
+        // Insere o brinquedo na posicao correta ordenada
+        lista->elementos[j + 1] = chave;
+    }
+}
+
+bool PodeTrocar(BRINQUEDO *item1, BRINQUEDO *item2){
+    if(item1 && item2){
+        // Verifica a ordem alfabetica da cor
+        switch(strncmp(item1->cor, item2->cor, 10)){
+            case 1: return true; break;
+            case -1: return false; break;
+        }
+
+        // Verifica o comprimento
+        if(item1->comprimento > item2->comprimento)
+            return true;
+        else if(item1->comprimento < item2->comprimento)
+            return false;
+        
+        // Verifica a nota
+        if(item1->nota < item2->nota)
+            return true;
+        
+        // Se for igual ou estiver na ordem
+        return false;
+    }
+    return false;
+}
+
+LISTA *ListaCriar(int brinquedos){
     LISTA *lista = (LISTA *)calloc(1, sizeof(LISTA));
-    lista->tamanho = 0;
-    lista->inicio = NULL;
-    lista->fim = NULL;
+    lista->elementos = (BRINQUEDO **)calloc(brinquedos, sizeof(BRINQUEDO *));
+    if(lista->elementos == NULL){
+        printf("Falta memoria!\n");
+        free(lista);
+        lista = NULL;
+
+        return NULL;
+    }
+    lista->tamanho = brinquedos;
 
     return lista;
 }
 
-LISTA *lista_adicionar(LISTA *lista, ITEM *item)
+LISTA *ListaAdicionar(LISTA *lista, BRINQUEDO *item, int posicao)
 {
-    if (lista && item)
-    {
-        if (lista->tamanho == 0)
-        {
-            lista->inicio = item;
-            lista->fim = item;
-        }
-        else
-        {
-            item->anterior = lista->fim;
-            lista->fim->proximo = item;
-            lista->fim = item;
-        }
-        lista->tamanho++;
+    if(lista != NULL && item != NULL){
+        lista->elementos[posicao] = item;
         return lista;
     }
-
     return NULL;
 }
 
-ITEM *item_criar(int id, char *cor, float comprimento, float nota)
+void ListaFree(LISTA **lista){
+    if(lista && *lista){
+        for(int i = 0; i < (*lista)->tamanho; i++){
+            free((*lista)->elementos[i]);
+        }
+        free((*lista)->elementos);
+        free(*lista);
+        *lista = NULL;
+    }
+}
+
+BRINQUEDO *ItemCriar(int id, char *cor, float comprimento, float nota)
 {
-    ITEM *item = (ITEM *)calloc(1, sizeof(ITEM));
-    item->proximo = NULL;
-    item->anterior = NULL;
+    BRINQUEDO *item = (BRINQUEDO *)calloc(1, sizeof(BRINQUEDO));
     item->id = id;
     strcpy(item->cor, cor);
     item->comprimento = comprimento;
@@ -97,15 +249,3 @@ ITEM *item_criar(int id, char *cor, float comprimento, float nota)
     return item;
 }
 
-void BubbleSort(LISTA *lista)
-{
-    ITEM *itemAuxTroca = NULL;
-    ITEM *itemAux = lista->inicio;
-    bool estaOrdenado = false;
-    for (int i = 0; i < lista->tamanho && !estaOrdenado; i++)
-    {
-        for (int j = 0; j < lista->tamanho - 1; j++)
-        {
-        }
-    }
-}
