86f62fad8c39b4afda75658cf6f4af797933eb00
pedroYamacita <pedro.teidi@gmail.com>
2025-10-17 14:42:26 -0300

    add/util.h

---
+++ Avaliativa5/Entrega5.c
@@ -0,0 +1,17 @@
+#include "util.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main(){
+    int tamanho;
+    int vetor[1000000000];
+    
+    scanf(" %d", &tamanho);
+    
+    char* formato;
+    scanf(" %s", formato);
+
+    if (strncmp(formato, "random", 6)==0){
+        for
+    }
+}
